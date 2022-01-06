469a2a914c946d2ce63d26893ceb1d73d6dc4579
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-01-06 15:25:42 +0300

    edited

--- ToDoList 3/3.js
+++ ToDoList 3/3.js
@@ -1,46 +1,26 @@
-const inputEnteredData = document.querySelector("#input");
+const input = document.querySelector("#input");
 const addButton = document.querySelector("#inputButton");
-const todolist = document.querySelector("list")
+const todolist = document.querySelector("list");
 
 
-//inputtan verileri çekme
-function inputData() {
-  
-      // let li = document.createElement("li");
-      // ul.appendChild(li);
-      // let span = document.createElement("span");
-      // li.appendChild(span);
-      // let closeSpan = document.createElement("span")
-      // li.appendChild(closeSpan);
-      let newLiTag = "";
-      userData = [input.value];
-      userData.forEach( (element) => {
-        newLiTag += `<li> ${element} <span class ="icon" ><i class="fa fa-trash"></i></li>` ;
-      });
-      list.innerHTML = newLiTag; //ul'e (id:list) li tag ekler
-      input.value =""; //giriş yapıldıktan sonra input'u boşaltmak için
-}
-
-function lclStorage () {
-  let userData = inputEnteredData.value;
-  let getLocalStorageData = localStorage.getItem("Yeni Giriş"); // veriyi getirmek için kullandık
-  if (getLocalStorageData == null) {
-    listArray = [];
-  } else {
-    listArray =JSON.parse(getLocalStorageData); //json string'i bir js nesnesine dönüştürme
-  }
-    listArray.push(userData);
-    localStorage.setItem("Yeni Giriş", JSON.stringify(listArray)); // JavaScript nesnesini JSON olarak çevirir  
-}
-
-function additionButton () {
-  let inputEnteredData = input.value;
-  if (inputEnteredData != "" && inputEnteredData != " ") {
-    inputData();
-    lclStorage (inputEnteredData.value);
-  } else {
-    return;
+//input'a veri oluşturma
+function createNewElement() {
+  if (input.value != "" && input.value != " ") {
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    span.classList.add("icon"); // span'a icon klası eklendi.
+    span.innerHTML = `<i class="fa fa-trash"></i>`;
+    li.innerHTML = `<span>${input.value} </span> `;
+    li.appendChild(span);
+    list.appendChild(li);
+    input.value = "";
   }
 }
 
 
+//li'yi silmek için func.
+function deleteElement(e) {
+  if (e.target.className === "icon") {
+    e.target.parentElement.remove();
+  }
+}
