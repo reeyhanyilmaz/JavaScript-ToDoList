4e57a7ad996e03ce5507197da98d4c1dfd753952
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-01-06 17:12:59 +0300

    edited

--- ToDoList 3/3.js
+++ ToDoList 3/3.js
@@ -2,14 +2,14 @@ const input = document.querySelector("#input");
 const addButton = document.querySelector("#inputButton");
 const todolist = document.querySelector("list");
 
-
 //input'a veri oluşturma
 function createNewElement() {
   if (input.value != "" && input.value != " ") {
     let li = document.createElement("li");
     let span = document.createElement("span");
     span.classList.add("icon"); // span'a icon klası eklendi.
-    span.innerHTML = `<i class="fa fa-trash"></i>`;
+    span.innerHTML = `<i class="fa fa-trash" ></i> `;
+    span.addEventListener("click", deleteElement);
     li.innerHTML = `<span>${input.value} </span> `;
     li.appendChild(span);
     list.appendChild(li);
@@ -17,10 +17,11 @@ function createNewElement() {
   }
 }
 
-
 //li'yi silmek için func.
 function deleteElement(e) {
   if (e.target.className === "icon") {
-    e.target.parentElement.remove();
+    if (confirm("Silmek istediğinize emin misiniz?")) {
+      e.target.parentElement.remove();
+    }
   }
 }
